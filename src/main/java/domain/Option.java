f1a2105845d19a949fdac875220a5fa3a6830256
lowoon <sangbo0229@gmail.com>
2020-04-10 16:40:34 +0900

    feat: option 관련 기능 추가

--- src/main/java/domain/Option.java
+++ src/main/java/domain/Option.java
@@ -1,4 +1,35 @@
 package domain;
 
-public class Command {
+import java.util.Arrays;
+
+public enum Option {
+
+    ORDER(1),
+    PAY(2),
+    EXIT(3);
+
+    private int option;
+
+    Option(final int option) {
+        this.option = option;
+    }
+
+    public static Option from(final int optionNumber) {
+        return Arrays.stream(Option.values())
+                .filter(option -> option.option == optionNumber)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("주문등록(1), 결제하기(2), 프로그램 종료(3)만 선택 가능합니다."));
+    }
+
+    public boolean isOrder() {
+        return option == ORDER.option;
+    }
+
+    public boolean isPay() {
+        return option == PAY.option;
+    }
+
+    public boolean isNotExit() {
+        return option != EXIT.option;
+    }
 }
--- src/test/java/domain/OptionTest.java
+++ src/test/java/domain/OptionTest.java
@@ -1,5 +1,65 @@
+package domain;
+
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class OptionTest {
 
+    @Test
+    void create() {
+        Option option = Option.from(1);
+        assertThat(option).isNotNull();
+    }
+
+    @Test
+    void createWithInvalidInput() {
+        assertThatThrownBy(() -> Option.from(4))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문등록(1), 결제하기(2), 프로그램 종료(3)만 선택 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("createOptionOrder")
+    void isOrder(Option option, boolean expected) {
+        assertThat(option.isOrder()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createOptionOrder() {
+        return Stream.of(
+                Arguments.of(Option.from(1), true),
+                Arguments.of(Option.from(2), false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createOptionPay")
+    void isPay(Option option, boolean expected) {
+        assertThat(option.isPay()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createOptionPay() {
+        return Stream.of(
+                Arguments.of(Option.from(2), true),
+                Arguments.of(Option.from(1), false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createOptionExit")
+    void isNotExit(Option option, boolean expected) {
+        assertThat(option.isNotExit()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createOptionExit() {
+        return Stream.of(
+                Arguments.of(Option.from(2), true),
+                Arguments.of(Option.from(3), false)
+        );
+    }
 }
