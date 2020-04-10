3330bbaecc86f9fb9b79d388259b295d06926a79
lowoon <sangbo0229@gmail.com>
2020-04-10 16:39:57 +0900

    feat: order 관련 기능 추가

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -1,4 +1,26 @@
 package domain;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Order {
+
+    private static Map<Table, Menus> orders;
+
+    static {
+        orders = new HashMap<>();
+        for (Table table : TableRepository.tables()) {
+            orders.put(table, new Menus(Collections.EMPTY_LIST));
+        }
+    }
+
+    public void addOrder(Table table, Menu menu, int count) {
+        Menus menus = orders.get(table);
+        menus.add(menu, count);
+    }
+
+    public Map<Table, Menus> getOrders() {
+        return orders;
+    }
 }
--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -1,5 +1,21 @@
+package domain;
+
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        order = new Order();
+    }
+
+    @Test
+    void create() {
+        assertThat(order.getOrders().values().size()).isEqualTo(TableRepository.tables().size());
+    }
 }
